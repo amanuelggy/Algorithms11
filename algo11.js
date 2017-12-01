2745441ac1c77ca4a97375a6a2d91e14e609eab0
Amanual <amanual2@gmail.com>
2017-12-01 12:12:30 -0500

    algo11

---
+++ algo11.js
@@ -0,0 +1,17 @@
+// Max, Min, and Average
+var array = [-10, 1, 2, 3, -5, 7, 3, 4, -3, 5, 8, -6, 9, 10, -9, 30];
+var max = 0;
+var min = 0;
+var sum = 0;
+var ave = 0;
+for (var i = 0; i < array.length; i++) {
+    sum += array[i];
+    ave = sum / array.length;
+    if (array[i] > max) {
+        max = array[i];
+    }
+    if (array[i] < min) {
+        min = array[i];
+    }
+}
+console.log("max: " + max + ", min: " + min + ", ave: " + ave);
